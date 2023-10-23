7a6eb4e843476ba4763d72eef68566cf0da7e096
hayden.rear <hayden.rear@gmail.com>
2023-10-23 16:00:06 -0400

    validation test training

--- src/python_util/logger/logger.py
+++ src/python_util/logger/logger.py
@@ -1,4 +1,5 @@
 import logging
+import typing
 
 from python_util.logger.log_level import LogLevel
 
@@ -17,6 +18,20 @@ class LoggerFacade:
                 file.write('\n')
                 logging.debug(file_output)
                 print(f"Debug: {file_output}")
+
+    @staticmethod
+    def debug_deferred(file_output: typing.Callable[[], str], enable_debug: bool = False):
+        if enable_debug:
+            LogLevel.set_log_level(logging.DEBUG)
+            assert LogLevel.is_debug_enabled()
+        if LogLevel.is_debug_enabled():
+            with open('/Users/hayde/IdeaProjects/drools/feature-extractor/multi_modal/test_work/debug.log',
+                      'a') as file:
+                deferred_value = file_output()
+                file.write(deferred_value)
+                file.write('\n')
+                logging.debug(deferred_value)
+                print(f"Debug: {deferred_value}")
 
     @staticmethod
     def info(output):
