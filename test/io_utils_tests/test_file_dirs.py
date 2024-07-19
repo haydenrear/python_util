69496f0d950a27642c3d9a1957771d94067ecacc
hayden.rear <hayden.rear@gmail.com>
2024-07-19 19:40:10 -0400

    adding authorization server and commit-diff-context

--- test/io_utils_tests/test_file_dirs.py
+++ test/io_utils_tests/test_file_dirs.py
@@ -1,8 +1,9 @@
 import os.path
 from unittest import TestCase
 
-from python_util.io_utils.file_dirs import iterate_files_in_directories, get_base_path_of_current_file, get_test_work_dir, \
-    delete_files_and_dirs_recursively
+from python_util.io_utils.file_dirs import iterate_files_in_directories, get_base_path_of_current_file, \
+    get_test_work_dir, \
+    delete_files_and_dirs_recursively, recursive_dir_iter
 from python_util.io_utils.io import create_file
 
 
@@ -33,3 +34,17 @@ class Test(TestCase):
             for j in range(10):
                 to_create = os.path.join(test_work, str(i), str(j))
                 assert not os.path.exists(to_create)
+
+    def test_read_recursively(self):
+        count = 0
+        for f in filter(lambda x: any([x.endswith(y) for y in ['.java', 'gradle.kts', '.py']]) and all([y not in x for y in ['phx']]), recursive_dir_iter('../../commit-diff-context')):
+            if not os.path.isdir(f):
+                print("opening " + f)
+                with open(f, 'r') as to_read:
+                    try:
+                        for lin in to_read:
+                            for c in lin:
+                                count += 1
+                    except UnicodeDecodeError as u:
+                        print(u)
+        print(count)
